4805e750920e44a4b8943a249fa581dfd773914b
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-02 17:12:43 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n%2 == 0){
+            for(int i=n; i>=2; i-=2){
+                answer += i*i;
+            }             
+        }else{
+            for(int i=n; i>=1; i-=2){
+                answer += i;
+            }   
+        }
+        return answer;
+    }
+}
