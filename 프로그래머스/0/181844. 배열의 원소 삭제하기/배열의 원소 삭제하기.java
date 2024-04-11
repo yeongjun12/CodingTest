552a3e945416d2540e552c5d4815483dfcdad422
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-11 15:49:49 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.07 ms, Memory: 68.5 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        
+       for(int n : arr){
+           list.add(n);
+       }
+        
+       for(int n : delete_list){
+           list.remove((Integer)n);
+       }
+        
+       int[] answer = new int[list.size()];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
