594905e24b4c17d780ca09c462c2c0d255377ed3
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-12 15:39:17 +0900

    [level 0] Title: ad 제거하기, Time: 0.88 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 
 class Solution {
-    public ArrayList<String>  solution(String[] strArr) {
-        String[] answer = {};
+    public String[]  solution(String[] strArr) {
+       
         ArrayList<String> list = new ArrayList<>();
         for(int i=0; i<strArr.length; i++){
             if(!strArr[i].contains("ad")){
                 list.add(strArr[i]);
             }
         }
+         String[] answer = new String[list.size()];
         
-        return list;
+         list.toArray(answer);
+        return answer;
     }
 }
