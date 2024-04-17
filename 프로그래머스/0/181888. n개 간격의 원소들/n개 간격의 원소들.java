e31a2208573b1d2ad1814eb2071cdb7917974670
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-17 15:36:08 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 
 class Solution {
-    public ArrayList solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length/n];
-          ArrayList<Integer> list = new ArrayList<>();
-        
-        
-        for(int i=0; i<num_list.length; i+=n){
-            list.add(num_list[i]);   
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>(); // ArrayList를 Integer 타입으로 선언
+
+        for (int i = 0; i < num_list.length; i += n) {
+            list.add(num_list[i]);
         }
-        return list;
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i); // ArrayList의 요소를 int 배열에 복사
+        }
+
+        return answer; // int[] 타입의 배열을 반환
     }
 }
