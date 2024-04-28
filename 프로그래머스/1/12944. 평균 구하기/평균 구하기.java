b6628c06e1e64cff960c27f73be77ce66e291f5e
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-28 12:30:26 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int num : arr){
+            answer += num;
+        }
+        return answer/arr.length;
+    }
+}
