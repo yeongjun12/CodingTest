fd958d53bfb63687f8b760dec3377e9481b17602
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-04-30 16:35:40 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+        for(int i=1; i<=count; i++ ){
+           sum += price * i; 
+        }
+        
+        answer = money < sum ? sum - money : 0;
+
+        return answer;
+    }
+}
