c3c210ee4274674173e7ac40d1ba835d14da8bce
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-05-03 17:20:08 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT
+        I.ANIMAL_ID
+        ,I.NAME
+FROM
+        ANIMAL_INS I, ANIMAL_OUTS O
+WHERE 1=1
+AND
+        I.ANIMAL_ID = O.ANIMAL_ID
+AND
+        I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME;
