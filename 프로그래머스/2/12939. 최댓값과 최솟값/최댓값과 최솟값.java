5cbd4d7d88173942185b475d1acb903c5f33dd3f
lee yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-06-05 10:19:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.83 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,23 +2,18 @@ class Solution {
     public String solution(String s) {
         String[] strArray = s.split(" ");
         
-        // 입력이 없는 경우 처리
-        if (strArray.length == 0) {
-            return "";
-        }
         
-        // 초기화를 첫 번째 값으로 설정
-        int min = Integer.MAX_VALUE; 
-        int max = Integer.MIN_VALUE;
+        int min = Integer.parseInt(strArray[0]); 
+        int max = Integer.parseInt(strArray[0]);
         
-        // 배열을 한 번만 반복하여 최소값과 최대값 찾기
-        for (String str : strArray) {
-            int num = Integer.parseInt(str);
-            min = Math.min(min, num);
-            max = Math.max(max, num);
-        }
+         for(String str : strArray){
+             int num = Integer.parseInt(str);
+             
+             min = Math.min(min, num);
+             max = Math.max(max, num);       
+         }
         
-        String answer = min + " " + max;
+        String answer = min+ " " + max;
         return answer;
     }
 }
