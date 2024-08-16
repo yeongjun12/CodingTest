af372acfc580cafcaa1510f4e288cfd66e764aa7
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-17 00:34:34 +0900

    [Bronze II] Title: 대표값2, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {		
+		
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		
+		for(int i = 0; i<5; i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+		
+		sc.close();
+	}
+	
+}
