46f99350da5629f232083c7d50400ec29e45f368
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-22 00:23:14 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 768 ms, Memory: 60524 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -9,34 +9,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-    	
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-    	Map<String,String> map = new HashMap<>();
-    	ArrayList<String> list = new ArrayList<>();
-    	
-    	int count = Integer.parseInt(br.readLine());
-        
-    	for(int i = 0; i < count; i++) {
-    		String[] strArr = br.readLine().split(" ");
-    		String name = strArr[0];
-    		String isOffDuty = strArr[1];
-    		map.put(name, isOffDuty);
-    	}
-    	
-    	for(Map.Entry<String, String> entry : map.entrySet()) {
-    		if(entry.getValue().equals("enter")) {
-    			list.add(entry.getKey());
-    		}
-    	}
-    	
-    	Collections.sort(list, Collections.reverseOrder());
-    	
-    	for(String s : list) System.out.println(s);
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            Map<String, String> map = new HashMap<>();
+            ArrayList<String> list = new ArrayList<>();
+
+            int count = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < count; i++) {
+                String[] strArr = br.readLine().split(" ");
+                String name = strArr[0];
+                String isOffDuty = strArr[1];
+                map.put(name, isOffDuty);
+            }
+
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                if ("enter".equals(entry.getValue())) {
+                    list.add(entry.getKey());
+                }
+            }
+
+            Collections.sort(list, Collections.reverseOrder());
+
+            for (String s : list) {
+                wr.write(s + "\n");
+            }
+
+            wr.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
