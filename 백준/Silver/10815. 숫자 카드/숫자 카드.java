66d2a58ac96a8c9feb63cda9f84862684c95bbb0
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-22 18:56:07 +0900

    [Silver V] Title: 숫자 카드, Time: 1200 ms, Memory: 141072 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,54 +4,60 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-		static int N, M;
-		static int[] NArray;
-	
+	static int N, M;
+	static int[] NArray;
+	static int[] MArray;
     public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        
-         N = Integer.parseInt(br.readLine());
-         NArray = new int[N];
-        String[] strArray = br.readLine().split(" ");
-        
-        for(int i = 0; i < N; i++) {
-        	NArray[i] = Integer.parseInt(strArray[i]);
-        }
-        
-        Arrays.sort(NArray);
-        
-         M = Integer.parseInt(br.readLine());
-         int[] MArray = new int[M];
-         String[] mStrArr = br.readLine().split(" ");
-         
-         for(int i = 0; i < M; i++) {
-        	 int result = BinarySearch(Integer.parseInt(mStrArr[i]));
-        	 sb.append(result+" ");
-         }
-         System.out.println(sb.toString());
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+       StringBuilder sb = new StringBuilder();
+       N = Integer.parseInt(br.readLine());
+       NArray = new int[N];
+       String[] NstrArr = br.readLine().split(" ");
+       
+       for(int i = 0; i < N; i++) {
+    	   NArray[i] = Integer.parseInt(NstrArr[i]);
+       }
+       
+       Arrays.sort(NArray);
+       
+       M = Integer.parseInt(br.readLine());
+       int[] MArray = new int[M];
+       String[] MstrArr = br.readLine().split(" ");
+       
+       for(int i = 0; i < M; i++) {
+    	   int num = Integer.parseInt(MstrArr[i]);
+    	   sb.append(BinarySearch(num)+" ");
+       }
+       
+       wr.write(sb.toString().trim());
+       wr.flush();
+       wr.close();
+       br.close();
+       
     }
     
     public static int BinarySearch(int num) {
     	int left = 0;
-    	int right = N - 1;
+    	int right = N-1;
     	
-    	while(left <= right) {
+    	while(left<=right) {
     		int middle = (left+right)/2;
     		int middleValue = NArray[middle];
     		
     		if(num > middleValue) {
     			left = middle + 1;
-    		}else if(num < middleValue) {
+    		}else if(num < middleValue){
     			right = middle - 1;
-    		}else { // num == middleValue 값을 찾은경우
+    		}else {
     			return 1;
     		}
     	}
-    	return 0; //상근이의 카드에 num이 없을 경우
+    	return 0;
     }
 }
