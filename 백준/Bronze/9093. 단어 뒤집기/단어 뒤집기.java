1edf443a9485a0c4ecf983c80bba6dd4142bf296
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-24 15:15:22 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 420 ms, Memory: 51380 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,25 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-    	Scanner sc = new Scanner(System.in);
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
     
-    	int n = sc.nextInt();
-    	sc.nextLine(); // nextInt() 호출 후 남아 있는 개행 문자 제거
+    	int n = Integer.parseInt(br.readLine());
     	
     	for(int i = 0 ; i < n; i++) {
-    		String[] words = sc.nextLine().split(" ");
+    		String[] words = br.readLine().split(" ");
     		
     		for(String word : words) {
     			StringBuilder sb = new StringBuilder();
     			sb.append(word);
-    			System.out.print(sb.reverse().toString()+" ");
+    			wr.write(sb.reverse()+ " ");
     		}
-    		System.out.println();
+    		wr.write("\n");
     	}
     	
-    	sc.close();
+    	wr.flush();
+    	wr.close();
+    	br.close();
     }
     
 }
