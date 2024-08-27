3b766a9f8563f360dce2905a31c1465f3f2b19e3
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-27 14:52:57 +0900

    [Silver IV] Title: 큐 2, Time: 1572 ms, Memory: 408772 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	
+	static Queue<Integer> q = new LinkedList<>();	
+		
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	
+    	int count = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0; i < count; i++) {
+    		String[] strArr = br.readLine().split(" ");
+    		String str = strArr[0];
+    		int num = (strArr.length > 1) ? Integer.parseInt(strArr[1]) : -1;
+    		
+    		switch(str) {
+    			
+    			case "push" : push(num);
+    				break;
+    			case "pop" : wr.write(pop()+"\n");
+    				break;
+    			case "size" : wr.write(size()+"\n");
+    				break;
+    			case "empty" : wr.write(empty()+"\n");
+    				break;
+    			case "front" : wr.write(front()+"\n");
+    				break;
+    			case "back" : wr.write(back()+"\n");
+    		}
+    	}
+    	wr.flush();
+    	wr.close();
+    	br.close();
+    }
+    
+    public static void push(int num) {
+    	q.add(num);
+    }
+    
+    public static int pop() {
+    	return (q.isEmpty()) ? -1: q.remove();
+    }
+    
+    public static int size() {
+    	return q.size();
+    }
+    
+    public static int empty() {
+    	return (q.isEmpty())? 1 : 0;
+    }
+    
+    public static int front() {
+    	return (q.isEmpty()) ? -1 : q.peek();
+    }
+    
+    public static int back() {
+    	return (q.isEmpty()) ? -1 : ((LinkedList<Integer>) q).getLast();
+    }
+}
