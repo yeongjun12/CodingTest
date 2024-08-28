9c16f3aa376b4506d8d82fd188b1690e5df50bf9
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-29 00:13:12 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -16,6 +16,9 @@ public class Main {
         int size = Integer.parseInt(strArr[0]);  // 사람의 수 N
         int step = Integer.parseInt(strArr[1]);  // 제거할 순번 K
         
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
         ArrayList<Integer> list = new ArrayList<>();
         
         // 1부터 size까지 리스트에 추가
@@ -24,23 +27,22 @@ public class Main {
         }
         
         int index = 0;
-        wr.write("<");
-        
         // 리스트에서 사람을 제거할 때까지 반복
         while (list.size() > 0) {
             // 현재 인덱스에서 (K-1)만큼 이동. 모듈러 연산으로 순환을 유지.
             index = (index + step - 1) % list.size();
-            // 제거된 사람을 BufferedWriter에 직접 추가
-            wr.write(String.valueOf(list.remove(index)));
+            // 제거된 사람을 StringBuilder에 추가
+            sb.append(list.remove(index));
             
             if (list.size() > 0) {
-                wr.write(", ");
+                sb.append(", ");
             }
         }
         
-        wr.write(">\n");
+        sb.append(">");
+        // 결과 출력
+        wr.write(sb.toString() + "\n");
         
-        // 결과 출력 및 자원 정리
         wr.flush();
         wr.close();
         br.close();
