fafb4e4783cc5dc0a7ca8fb28d14ace84e0b5cdc
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-08-31 00:37:32 +0900

    [Silver IV] Title: 큐, Time: 176 ms, Memory: 20764 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -3,11 +3,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
  
 public class Main {
-	static Queue<Integer> q = new LinkedList<>();
+	static Deque<Integer> q = new LinkedList<>();
 	
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -72,6 +72,7 @@ public class Main {
     }
     
     public static int back() {
-    	return (q.isEmpty()) ? -1 : ((LinkedList<Integer>) q).getLast();
+    	//return (q.isEmpty()) ? -1 : ((LinkedList<Integer>) q).getLast();
+    	return (q.isEmpty()) ? -1 : q.getLast();
     }
     }
