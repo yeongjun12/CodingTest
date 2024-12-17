ce5f30b942bfbca68d1bd9af6e840e7790f5e84f
yeong jun <121650480+yeongjun12@users.noreply.github.com>
2024-12-18 00:34:28 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 188 ms, Memory: 18336 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,29 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int arr[] = {1,1,2,2,2,8};
-		int answer[] = new int[6];
-		
-		for(int i = 0; i<6; i++) {
-			answer[i] = sc.nextInt();
-		}
-		
-		for(int i = 0; i<6; i++) {
-			
-			if(arr[i] > answer[i]) {
-				System.out.print(arr[i]-answer[i]+ " ");
-			}else if(arr[i] < answer[i]) {
-				System.out.print(arr[i]-answer[i]+ " ");
-			}else {
-				System.out.print(0+" ");
-			}
-		}
-		
-		sc.close();
-	
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {1,1,2,2,2,8};
+        int arr2[] = new int[6];
+        
+        
+        for(int i = 0; i < 6; i++) {
+        	arr2[i] = sc.nextInt();
+        	
+        	System.out.print( arr[i]-arr2[i] + " ");
+        }
+        
+        
+    }
 }
